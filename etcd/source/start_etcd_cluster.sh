ebc6b68fae01e351d8dbe2fbc465ebe2770ad439
adsonwang <adsonwang@tencent.com>
2019-12-31 19:39:27 +0800

    第三章，etcd客户端命令etcdctl的使用

--- etcd/source/start_etcd_cluster.sh
+++ etcd/source/start_etcd_cluster.sh
@@ -10,6 +10,7 @@ NAME_1=m1
 NAME_2=m2
 NAME_3=m3
 
+#在同一个机器可以用不同的端口
 HOST_1=127.0.0.1
 HOST_2=127.0.0.1
 HOST_3=127.0.0.1
@@ -24,28 +25,41 @@ PEER_PORT_3=2384
 DATA1=data1
 DATA2=data2
 DATA3=data3
-CLUSTER=${NAME_1}=http://${HOST_1}:$PEER_PORT_1,${NAME_2}=http://${HOST_2}:$PEER_PORT_2,${NAME_3}=http://${HOST_3}:$PEER_PORT_3
+CLUSTER=${NAME_1}="http://${HOST_1}:$PEER_PORT_1,\
+${NAME_2}=http://${HOST_2}:$PEER_PORT_2,\
+${NAME_3}=http://${HOST_3}:$PEER_PORT_3"
 
 $etcd --data-dir=$DATA1 --name ${NAME_1} \
-    --initial-advertise-peer-urls http://${HOST_1}:$PEER_PORT_1 --listen-peer-urls http://$HOST_1:$PEER_PORT_1 \
-    --advertise-client-urls http://$HOST_1:$CLIENT_PORT_1 --listen-client-urls http://${HOST_1}:$CLIENT_PORT_1 \
+    --initial-advertise-peer-urls http://${HOST_1}:$PEER_PORT_1 \
+    --listen-peer-urls http://$HOST_1:$PEER_PORT_1 \
+    --advertise-client-urls http://$HOST_1:$CLIENT_PORT_1 \
+    --listen-client-urls http://${HOST_1}:$CLIENT_PORT_1 \
     --initial-cluster ${CLUSTER} \
-    --initial-cluster-state ${CLUSTER_STATE} --initial-cluster-token ${TOKEN} &
+    --initial-cluster-state ${CLUSTER_STATE} \
+    --initial-cluster-token ${TOKEN} &
 
 $etcd --data-dir=$DATA2 --name ${NAME_2} \
-    --initial-advertise-peer-urls http://${HOST_2}:$PEER_PORT_2 --listen-peer-urls http://$HOST_2:$PEER_PORT_2 \
-    --advertise-client-urls http://$HOST_2:$CLIENT_PORT_2 --listen-client-urls http://${HOST_2}:$CLIENT_PORT_2 \
+    --initial-advertise-peer-urls http://${HOST_2}:$PEER_PORT_2 \
+    --listen-peer-urls http://$HOST_2:$PEER_PORT_2 \
+    --advertise-client-urls http://$HOST_2:$CLIENT_PORT_2 \
+    --listen-client-urls http://${HOST_2}:$CLIENT_PORT_2 \
     --initial-cluster ${CLUSTER} \
-    --initial-cluster-state ${CLUSTER_STATE} --initial-cluster-token ${TOKEN} &
+    --initial-cluster-state ${CLUSTER_STATE} \
+    --initial-cluster-token ${TOKEN} &
 
 $etcd --data-dir=$DATA3 --name ${NAME_3} \
-    --initial-advertise-peer-urls http://${HOST_3}:$PEER_PORT_3 --listen-peer-urls http://$HOST_3:$PEER_PORT_3 \
-    --advertise-client-urls http://$HOST_3:$CLIENT_PORT_3 --listen-client-urls http://${HOST_3}:$CLIENT_PORT_3 \
+    --initial-advertise-peer-urls http://${HOST_3}:$PEER_PORT_3 \
+    --listen-peer-urls http://$HOST_3:$PEER_PORT_3 \
+    --advertise-client-urls http://$HOST_3:$CLIENT_PORT_3 \
+    --listen-client-urls http://${HOST_3}:$CLIENT_PORT_3 \
     --initial-cluster ${CLUSTER} \
-    --initial-cluster-state ${CLUSTER_STATE} --initial-cluster-token ${TOKEN} &
+    --initial-cluster-state ${CLUSTER_STATE} \
+    --initial-cluster-token ${TOKEN} &
 
 export ETCDCTL_API=3
-export ENDPOINTS=$HOST_1:$CLIENT_PORT_1,$HOST_2:$CLIENT_PORT_2,$HOST_3:$CLIENT_PORT_3
+export ENDPOINTS="$HOST_1:$CLIENT_PORT_1,\
+$HOST_2:$CLIENT_PORT_2,\
+$HOST_3:$CLIENT_PORT_3"
 
 echo waiting for etcd server cluster init ...
 sleep 5
